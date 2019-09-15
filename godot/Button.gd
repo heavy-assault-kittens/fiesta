89ea37110dbfd3d142072c6bae79b481ab326ffd
CrabbieMike <CrabbieMike@users.noreply.github.com>
2019-09-14 20:39:46 -0400

    Laid down the frame work for the main menu

---
+++ godot/Button.gd
@@ -0,0 +1,23 @@
+extends Button
+
+var a = 0
+var easy = 1
+var cruel = 2
+var multiple = 3
+var audio = 4
+var picture = 5
+var mii = 6
+onready var first_button = get_node("Button")
+
+func _randomize_my_variable():
+	a = randi()%7 + 1
+	if a == easy:
+		print ("Easy Question")
+	elif a == multiple:
+		print ("Multiple Choice")
+	elif a == audio:
+		print ("Rythem Hero")
+	elif a == picture:
+		print ("Fatal Frame")
+	elif a == mii:
+		print ("Find Mii")
