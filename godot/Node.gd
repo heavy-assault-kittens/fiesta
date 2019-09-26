cc57f6ee8705058466c8e66aa48e24cff5ffeb86
CrabbieMike <CrabbieMike@users.noreply.github.com>
2019-09-26 14:43:07 -0400

    Finished Fatal Frame

--- godot/Node.gd
+++ godot/Node.gd
@@ -3,10 +3,13 @@ extends Node
 var ace = 1
 var menu = 2
 var ann = 3
+var lan = 4
+var polite = 5
+var slug = 6
 
 func _on_Node_ready():
 	randomize()
-	var frame = randi()%4+1
+	var frame = randi()%7+1
 	if frame == ace:
 		print("ace")
 		$Ace.show()
@@ -16,6 +19,15 @@ func _on_Node_ready():
 	elif frame == menu:
 		print("menu")
 		$Persona_Menu.show()
+	elif frame == lan:
+		print("lan")
+		$Lan.show()
+	elif frame == polite:
+		print("polite")
+		$Polite.show()
+	elif frame == slug:
+		print("slug")
+		$Slug.show()
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Main_Menu.tscn")
