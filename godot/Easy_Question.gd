83141eda613679f6e817d53a23055407cb8952d9
CrabbieMike <CrabbieMike@users.noreply.github.com>
2019-10-07 17:23:39 -0400

    Added Reveal Answer Button to Easy Question and worked on multiple choice

--- godot/Easy_Question.gd
+++ godot/Easy_Question.gd
@@ -1,6 +1,24 @@
 extends Node
 
-
+var mario = "Who is the main villain of the Mario series?"
+var zap = "What is the name of the NES gun controller?"
+var kingdom = "Where do the characters in the Mario universe live?"
+var sonic = "What is the main color of Sonic's shoes?"
+var pokemon = "What were the first two Pokémon games in North America?"
+var olympics = "Which character was created first, Mario or Sonic?"
+var rings = "How many rings do you need to collect to get an extra life in most Sonic games?"
+var egg = "What is the name of Sonic the Hedgehog's arch-nemesis?"
+var pac = "What color is Pac-Man?"
+var emeralds = "How many Chaos Emeralds are there in most Sonic games?"
+var smash = "Who created Mario, Donkey Kong, and many more Nintendo characters?"
+var ps = "What are the four symbols on the face buttons of Playstation controllers?"
+var hedge = "What species of animal are Sonic, Amy, and Shadow?"
+var coins = "How many coins do you need to collect in Super Mario Bros. to earn an extra life?"
+var oneup = "In Super Mario Bros. for the NES, which stage has the first 1-Up Mushroom?"
+var nes = "What are the two face buttons on an NES controller?"
+var yoshi = "What is the name of the green dinosaur Mario rode in Super Mario World?"
+var party = "How many classic Mario Party games are there in Mario Party: The Top 100?"
+var bananas = "What do you collect 100 of to get an extra life in the Donkey Kong Country series of games?"
 
 func _on_Node_ready():
 	randomize()
@@ -24,19 +42,58 @@ func _on_Node_ready():
 	words.append ("What is the name of the green dinosaur Mario rode in Super Mario World?")
 	words.append ("How many classic Mario Party games are there in Mario Party: The Top 100?")
 	words.append ("What do you collect 100 of to get an extra life in the Donkey Kong Country series of games?")
-	words.append ("What is the name of Sonic the Hedgehog's arch-nemesis?")
 	var word_index = randi() % words.size()
 	$Label.text = words[word_index]
+	
+	var label = $Label.text
+	
+	if label == mario:
+		$Label2.text = "Bowser"
+	elif label == zap:
+		$Label2.text = "The Zapper"
+	elif label == kingdom:
+		$Label2.text = "Mushroom Kingdom"
+	elif label == sonic:
+		$Label2.text = "Red"
+	elif label == pokemon:
+		$Label2.text = "Red and Blue"
+	elif label == olympics:
+		$Label2.text = "Mario"
+	elif label == rings:
+		$Label2.text = "100"
+	elif label == egg:
+		$Label2.text = "Egg-Man"
+	elif label == pac:
+		$Label2.text = "Yellow"
+	elif label == emeralds:
+		$Label2.text = "7"
+	elif label == smash:
+		$Label2.text = "Shigeru Miyamoto"
+	elif label == ps:
+		$Label2.text = "Cross, Circle, Square and Triangle"
+	elif label == hedge:
+		$Label2.text = "Hedgehogs"
+	elif label == coins:
+		$Label2.text = "100"
+	elif label == oneup:
+		$Label2.text = "World 1-1"
+	elif label == nes:
+		$Label2.text = "A and B"
+	elif label == yoshi:
+		$Label2.text = "Yoshi"
+	elif label == party:
+		$Label2.text = "100"
+	elif label == bananas:
+		$Label2.text = "Bananas"
 
-func score_update():
-	score += 1
-	return str(score)
 
 func _on_Button_pressed():
-	$GridContainer3/Score_Marker.text = score_update()
 	get_tree().change_scene("res://Main_Menu.tscn")
 
 func _on_Button2_pressed():
 	get_tree().change_scene("res://Main_Menu.tscn")
 
-#code by Michael Crabbe
+func _on_Answer_pressed():
+	$Answer.hide()
+
+# Code By Michael Crabbe
--- godot/Multiple_Choice.gd
+++ godot/Multiple_Choice.gd
@@ -3,10 +3,12 @@ extends Node
 var Pokemon = 1
 var fox = 2
 var Ink = 3
+var zelda = 4
+var yoshi = 5
 
 func _on_Node_ready():
 	randomize()
-	var text = randi()%4+1
+	var text = randi()%6+1
 	if text == Pokemon:
 		print("pokemon")
 		$Question.text = ("Which Pokémon can you get by evolving Nincada?")
@@ -31,6 +33,22 @@ func _on_Node_ready():
 		$Star_Fox/Aileron.text = ("Aileron Roll")
 		$Star_Fox/Inside.text = ("Inside Loop")
 		$Star_Fox.show()
+	elif text == zelda:
+		print("zelda")
+		$Question.text = ("In The Legend of Zelda: Tri Force Heroes, what is the lowest number of Heart Containers you can have?")
+		$Zelda/Nine.text = ("Nine")
+		$Zelda/Three.text = ("Three")
+		$Zelda/Eight.text = ("Eight")
+		$Zelda/Six.text = ("Six")
+		$Zelda.show()
+	elif text == yoshi:
+		print("yoshi")
+		$Question.text = ("Which of these games did the creator of Tetris work on?")
+		$Yoshi/Cookie.text = ("Yoshi's Cookie")
+		$Yoshi/Puyo.text = ("Puyo Puyo")
+		$Yoshi/Atack.text = ("Tetris Attack")
+		$Yoshi/Turbo.text = ("Super Puzzle Fighter II Turbo")
+		$Yoshi.show()
 
 func _on_Shedinja_pressed():
 	get_tree().change_scene("res://Main_Menu.tscn")
@@ -68,4 +86,26 @@ func _on_Ink_pressed():
 func _on_Squid_pressed():
 	get_tree().change_scene("res://Main_Menu.tscn")
 
-#code by Michael Crabbe
+func _on_Nine_pressed():
+	$Wrong_Answer.text = ("Wrong Answer!")
+
+func _on_Three_pressed():
+	$Wrong_Answer.text = ("Wrong Answer!")
+
+func _on_Eight_pressed():
+	$Wrong_Answer.text = ("Wrong Answer!")
+
+func _on_Six_pressed():
+	get_tree().change_scene("res://Main_Menu.tscn")
+
+func _on_Cookie_pressed():
+	get_tree().change_scene("res://Main_Menu.tscn")
+
+func _on_Puyo_pressed():
+	$Wrong_Answer.text = ("Wrong Answer")
+
+func _on_Atack_pressed():
+	$Wrong_Answer.text = ("Wrong Answer")
+
+func _on_Turbo_pressed():
+	$Wrong_Answer.text = ("Wrong Answer")
